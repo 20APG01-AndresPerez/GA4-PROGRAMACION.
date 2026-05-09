c9e45ebdfe3020e41573fb076c978e3bbbdae59a
20APG01-AndresPerez <a.perez.5adb@gmail.com>
2026-05-09 01:02:29 -0600

    Add files via upload

---
+++ Act2_Inc2_Lecc11.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num = 1;
+
+        do
+        {
+            if (num % 2 != 0)
+            {
+                Console.WriteLine(num);
+            }
+            num++;
+        }while (num <100);
+     }
+}
---
+++ Act2_Inc2_Lecc11.psc
@@ -0,0 +1,16 @@
+Algoritmo Act2_Inc2_Lecc11
+	Definir num Como Entero
+	
+	num = 1
+	
+	Repetir
+		
+		Si num MOD 2 <> 0 Entonces
+			Escribir num
+		FinSi
+		
+		num = num + 1
+		
+	Hasta Que num > 100
+	
+FinAlgoritmo
