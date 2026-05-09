b202778c21687f313239d68ce1d19e7b99d32290
20APG01-AndresPerez <a.perez.5adb@gmail.com>
2026-05-09 00:11:00 -0600

    Add files via upload

---
+++ Act2_Inc2_Lecc10.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num;
+        int i = 2;
+
+        Console.WriteLine("Digite un numero: ");
+        num= Convert.ToInt32(Console.ReadLine());
+
+        for (i =2;i <= num; i = i + 2)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
---
+++ Act2_Inc2_Lecc10.psc
@@ -0,0 +1,14 @@
+Algoritmo Act2_Inc2_Lecc10
+	Definir num, i Como Entero
+	
+	Escribir "Digite un numero: "
+	Leer num
+	
+	i=2
+	
+	Para i <- 2 Hasta num Con Paso 2 Hacer
+		
+		Escribir i
+		
+	Fin Para
+FinAlgoritmo
