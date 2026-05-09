5557ba38da0f0220c5eca1149dc377f6775e9ce0
20APG01-AndresPerez <a.perez.5adb@gmail.com>
2026-05-08 22:23:41 -0600

    Add files via upload

---
+++ Act1_Inc1_Lecc9.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int pesotot, pesoman, cantman;
+        pesotot = 0;
+        pesoman = 0;
+        cantman = 0;
+        while (pesotot<1000)
+        {
+            Console.WriteLine("Digite el peso de las manzanas: ");
+            pesoman= Convert.ToInt32(Console.ReadLine());
+
+            pesotot = pesotot + pesoman;
+            cantman++;
+
+            Console.WriteLine("El peso que llevas es: "+pesotot+" y la cantidad que llevas es: "+cantman);
+        }
+        Console.WriteLine("Llegaste al limite de peso");
+        Console.WriteLine("En total te llevaste "+cantman+" manzanas");
+    }
+}
---
+++ Act1_Inc1_Lecc9.psc
@@ -0,0 +1,19 @@
+Algoritmo Act1_Inc1_Lecc9
+	Definir pesotot, pesoman, cantman Como Entero
+	pesotot=0
+	pesoman=0
+	Mientras pesotot<1000 Hacer
+		Escribir "Ingrese el peso de la manzana: "
+		Leer pesoman
+		
+		pesotot=pesotot+pesoman
+		cantman= cantman+1
+		
+		Escribir "El peso de las manzanas es: ",pesotot," y la cantidad que llevas es: ",cantman
+	Fin Mientras
+	
+	Escribir "Llegaste al kilogramo de manzanas"
+	Escribir "La cantidad de manzanas que llevaste son: ",cantman
+	
+	
+FinAlgoritmo
