9765beb8f9ebb40a2790030a6c16fdb568c4b01a
20APG01-AndresPerez <a.perez.5adb@gmail.com>
2026-05-08 23:47:23 -0600

    Add files via upload

---
+++ Act1_Inc1_Lecc10.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int sello;
+
+        for (sello = 1; sello <= 8; sello++) 
+        {
+            Console.WriteLine("Visita numero "+ sello);
+            Console.WriteLine("Has obtenido un sello ");
+        }
+        Console.WriteLine("FELICIDADES, HAS OBTENIDO UN CORTE GRATIS");
+    }
+}
---
+++ Act1_Inc1_Lecc10.psc
@@ -0,0 +1,9 @@
+Algoritmo Act1_Inc1_Lecc10
+	Definir sello como Entero
+	
+	Para sello <-1 Hasta 8 Con Paso 1 Hacer
+		Escribir "Visita numero ", sello
+		Escribir "Has obtenido un sello "
+	Fin Para
+	Escribir "FELICIDADES, HAS OBTENIDO UN CORTE GRATIS"
+FinAlgoritmo
