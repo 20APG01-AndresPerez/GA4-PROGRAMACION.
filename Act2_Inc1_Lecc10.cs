2a44ee470957d403da733c5176dab3bbb715dc80
20APG01-AndresPerez <a.perez.5adb@gmail.com>
2026-05-08 23:59:46 -0600

    Add files via upload

---
+++ Act2_Inc1_Lecc10.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num = 4;
+
+        for (num = 4; num <= 80; num=num+4) 
+        {
+            Console.WriteLine(num);
+        }
+
+    }
+}
---
+++ Act2_Inc1_Lecc10.psc
@@ -0,0 +1,8 @@
+Algoritmo Act2_Inc1_Lecc10
+	Definir num  Como Entero
+	num=4
+	
+	Para num<-4 Hasta 80 Con Paso 4 Hacer
+		Escribir "Los primeros multiplos son: ",num
+	Fin Para
+FinAlgoritmo
