1b816d5483612c833b513af4140ebb24478b0056
20APG01-AndresPerez <a.perez.5adb@gmail.com>
2026-05-08 23:24:40 -0600

    Add files via upload

---
+++ Act4_Inc1_Lecc9.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num;
+        int resultado;
+        int i = 1;
+
+        Console.WriteLine("Digite un numero: ");
+        num= Convert.ToInt32(Console.ReadLine());
+
+        while (i<=10)
+        {
+            resultado = num * i;
+            Console.WriteLine(+num+" x "+i+ " = "+ resultado );
+            i = i + 1;
+        }
+
+    }
+}
---
+++ Act4_Inc1_Lecc9.psc
@@ -0,0 +1,14 @@
+Algoritmo Act4_Inc1_Lecc9
+	Definir num, i, resultado Como Entero
+	Escribir "Ingrese un numero"
+	Leer num
+	
+	i=1
+	
+	Mientras i<=10 Hacer
+		resultado = num*i
+		Escribir num, " x ",i," =", resultado
+		i=i+1
+	Fin Mientras
+	
+FinAlgoritmo
